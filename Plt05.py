22d295fb27f5ad077980b45933655dc2095a22c1
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-05-07 18:41:40 -0300

    Add files via upload
    Este código cria um gráfico de linhas com pontos vermelhos e marcados com um círculo, fiz  isso utilizando a biblioteca Matplotlib.

---
+++ Plt05.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = [10,20,30,40,10]
+y = [20,40,10,80,100]
+plt.plot(x, color="red", marker="o")
+plt.plot(y, color="blue", marker="o")
+plt.title("Gráfico com Cores e Marcadores")
+plt.show()
